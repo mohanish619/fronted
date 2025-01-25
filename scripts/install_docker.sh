0139f3c6f18ccb1124482e1fe4d616186f6acdb6
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2025-01-25 18:03:34 +0530

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -5,9 +5,7 @@ echo "Installing Docker..."
 sudo yum update -y
 sudo yum install -y docker
 
-# Start and enable Docker service
-sudo systemctl start docker
-sudo systemctl enable docker
+
 
 # Verify Docker installation
 docker --version
