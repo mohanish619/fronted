73219ca3a7337c6bd5538417f8bd2569b3c345bc
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2025-01-25 15:45:16 +0530

    Update restart_docker.sh

--- scripts/restart_docker.sh
+++ scripts/restart_docker.sh
@@ -6,6 +6,6 @@ docker stop frontend-container
 docker rm frontend-container
 
 # Run the container again
-docker run -d -p 80:80 --name frontend-container frontend-image
+docker run -d -p 80:80 --name frontend-container 7276829907/frontend-image:latest
 
 echo "Frontend Docker container restarted!"
