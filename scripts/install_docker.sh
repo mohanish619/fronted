5d21951e613ad94e0da81a31d50206914ad766a0
mohanish619 <75253869+mohanish619@users.noreply.github.com>
2025-01-25 18:03:05 +0530

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -12,13 +12,4 @@ sudo systemctl enable docker
 # Verify Docker installation
 docker --version
 
-# Extract the build.zip file
-echo "Extracting the build.zip file..."
-mkdir -p /home/ec2-user/build
-unzip /home/ec2-user/build/build.zip -d /home/ec2-user/build/
 
-# Verify the files were extracted
-echo "Files extracted:"
-ls -l /home/ec2-user/build/
-
-# Add any additional steps if required, such as setting up Docker containers
